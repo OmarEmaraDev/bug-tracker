0bd517112a7a7042ad55d2817eb26ce86f8c6723
OmarEmaraDev <mail@OmarEmara.dev>
2019-11-28 20:40:11 +0200

    Added Log Out option.

--- src/dev.omaremara.bugtracker/dev/omaremara/bugtracker/controller/ReportListController.java
+++ src/dev.omaremara.bugtracker/dev/omaremara/bugtracker/controller/ReportListController.java
@@ -1,6 +1,7 @@
 package dev.omaremara.bugtracker.controller;
 
 import dev.omaremara.bugtracker.Main;
+import dev.omaremara.bugtracker.view.LoginView;
 import dev.omaremara.bugtracker.view.NewReportView;
 import javafx.event.ActionEvent;
 import javafx.scene.Scene;
@@ -11,5 +12,10 @@ public class ReportListController {
     Stage stage = Main.primaryStage;
     Scene newReportScene = new NewReportView().getScene();
     stage.setScene(newReportScene);
+  }
+  public void logOut(ActionEvent e) {
+    Stage stage = Main.primaryStage;
+    Scene loginScene = new LoginView().getScene();
+    stage.setScene(loginScene);
   }
 }
--- src/dev.omaremara.bugtracker/dev/omaremara/bugtracker/view/ReportListView.java
+++ src/dev.omaremara.bugtracker/dev/omaremara/bugtracker/view/ReportListView.java
@@ -5,12 +5,20 @@ import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
-import javafx.scene.control.Label;
+import javafx.scene.control.ButtonBar;
+import javafx.scene.control.ButtonBar.ButtonData;
 import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 
 public class ReportListView implements View {
   public Scene getScene() {
+    Button logOutButton = new Button("Log Out");
+    ButtonBar.setButtonData(logOutButton, ButtonData.RIGHT);
+
+    ButtonBar buttonBar = new ButtonBar();
+    buttonBar.setPadding(new Insets(20));
+    buttonBar.getButtons().addAll(logOutButton);
+
     Button newReportButton = new Button("New Report");
     BorderPane.setAlignment(newReportButton, Pos.BOTTOM_RIGHT);
     BorderPane.setMargin(newReportButton, new Insets(20));
@@ -18,9 +26,11 @@ public class ReportListView implements View {
 
     ReportListController controller = new ReportListController();
     newReportButton.setOnAction(e -> controller.newReport(e));
+    logOutButton.setOnAction(e -> controller.logOut(e));
 
     BorderPane borderPane = new BorderPane();
     borderPane.setBottom(newReportButton);
+    borderPane.setTop(buttonBar);
     return new Scene(borderPane);
   }
 }
