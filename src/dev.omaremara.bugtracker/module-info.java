5a79347456a4aaa3188a77e84517be8132570722
MOHAMEDALKHATEB <alkhateb118@gmail.com>
2019-11-30 04:15:00 +0200

    Add three files

--- src/dev.omaremara.bugtracker/module-info.java
+++ src/dev.omaremara.bugtracker/module-info.java
@@ -1,6 +1,6 @@
 module dev.omaremara.bugtracker {
-  requires javafx.base;
-  requires javafx.controls;
-  requires javafx.graphics;
-  exports dev.omaremara.bugtracker to javafx.graphics;
+    requires javafx.base;
+    requires javafx.controls;
+    requires javafx.graphics;
+    exports dev.omaremara.bugtracker to javafx.graphics;
 }
