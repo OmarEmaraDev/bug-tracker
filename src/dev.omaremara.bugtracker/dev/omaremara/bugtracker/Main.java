93a87de6a5daf0f361c8b66b89d19587614334b9
OmarEmaraDev <mail@OmarEmara.dev>
2019-12-05 21:06:25 +0200

    Remove redundant imports.

--- src/dev.omaremara.bugtracker/dev/omaremara/bugtracker/Main.java
+++ src/dev.omaremara.bugtracker/dev/omaremara/bugtracker/Main.java
@@ -1,8 +1,6 @@
 package dev.omaremara.bugtracker;
 
 import dev.omaremara.bugtracker.view.LoginView;
-import dev.omaremara.bugtracker.view.NewReportView;
-import dev.omaremara.bugtracker.view.ReportsView;
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
