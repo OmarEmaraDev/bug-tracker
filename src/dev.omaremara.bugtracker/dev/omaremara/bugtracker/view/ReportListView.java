7963e3a0fe98a7f06dc35440aa91e72a6bcbe93d
OmarEmaraDev <mail@OmarEmara.dev>
2019-12-11 13:49:54 +0200

    Refactor Report List UI.

--- src/dev.omaremara.bugtracker/dev/omaremara/bugtracker/view/ReportListView.java
+++ src/dev.omaremara.bugtracker/dev/omaremara/bugtracker/view/ReportListView.java
@@ -5,6 +5,7 @@ import dev.omaremara.bugtracker.model.Report;
 import java.time.format.DateTimeFormatter;
 import javafx.geometry.HPos;
 import javafx.geometry.Insets;
+import javafx.geometry.Orientation;
 import javafx.geometry.Pos;
 import javafx.geometry.VPos;
 import javafx.scene.Scene;
@@ -12,8 +13,13 @@ import javafx.scene.control.Button;
 import javafx.scene.control.ButtonBar;
 import javafx.scene.control.ButtonBar.ButtonData;
 import javafx.scene.control.Label;
+import javafx.scene.control.Separator;
+import javafx.scene.layout.Background;
+import javafx.scene.layout.BackgroundFill;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.CornerRadii;
 import javafx.scene.layout.GridPane;
+import javafx.scene.layout.Priority;
 import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
 import javafx.stage.Stage;
@@ -24,14 +30,13 @@ public class ReportListView implements View {
     ButtonBar.setButtonData(logOutButton, ButtonData.RIGHT);
 
     ButtonBar buttonBar = new ButtonBar();
+    buttonBar.setBackground(new Background(new BackgroundFill(
+        Color.web("#24292e"), CornerRadii.EMPTY, Insets.EMPTY)));
     buttonBar.setPadding(new Insets(20));
     buttonBar.getButtons().addAll(logOutButton);
 
     GridPane reportsGrid = new GridPane();
     reportsGrid.setPadding(new Insets(20));
-    reportsGrid.setAlignment(Pos.CENTER);
-    reportsGrid.setVgap(20);
-    reportsGrid.setHgap(40);
 
     int i = 0;
     for (Report report : ReportListController.getAllReports()) {
@@ -46,13 +51,18 @@ public class ReportListView implements View {
 
       VBox infoVBox = new VBox(titleLabel, infoLabel);
       GridPane.setHalignment(infoVBox, HPos.LEFT);
+      GridPane.setHgrow(infoVBox, Priority.ALWAYS);
 
       Button viewReportButton = new Button("View Report");
       GridPane.setHalignment(viewReportButton, HPos.RIGHT);
 
+      Separator separator = new Separator(Orientation.HORIZONTAL);
+      separator.setPadding(new Insets(10, 0, 10, 0));
+
       reportsGrid.add(infoVBox, 0, i);
       reportsGrid.add(viewReportButton, 1, i);
-      i++;
+      reportsGrid.add(separator, 0, i + 1, 2, 1);
+      i += 2;
     }
 
     Button newReportButton = new Button("New Report");
